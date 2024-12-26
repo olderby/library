cbf04ffb63c6e6fcf7407f6cd444f8758b648817
Oscar Derby <oscar.derby3@gmail.com>
2024-12-26 10:41:56 -0500

    Added methods to library class

--- library.py
+++ library.py
@@ -14,17 +14,32 @@ class Book:
 
 class Library:
 
-    def add_book(book):
-        pass #add a book object to a list of book objects
+    books = []
 
-    def borrow_book(title):
-        pass #pop a book object out of the list of book objects
+    def add_book(self, book):
+        #add a book object to a list of book objects
+        book_title = input("Enter book title:\t")
+        book_author = input("Enter book author:\t")
+        book_ISBN = input("Enter book ISBN:\t")
+        book = Book(book_title, book_author, book_ISBN)
+        self.books.append(book)
+
+    def borrow_book(self, title):
+        #if the title is in the library mark it as borrorwed
+        try:
+            for books in self.books:
+                if books.title == title:
+                    print("I found the book!")
+        except:
+            print("I cannot find that book")
 
     def return_book(title):
         pass #insert a book object to the list of book objects
 
-    def list_books():
-        pass #list all book objects by title in the list of book objects
+    def list_books(self):
+        #list all book objects by title in the list of book objects
+        for books in self.books:
+            print(books)
 
     def __init__(self, name, books = [""]):
         self.name = name
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import library
+from library import Book, Library
 import csv
 
 def main():
-    choice = menu()
 
-    if choice == '5':
-        print("Closing library!")
-        return 0
-    elif choice == '1':
-        pass
-    elif choice == '2':
-        pass
-    elif choice == '3':
-        pass
-    elif choice == '4':
-        pass
-    elif choice == '8':
-        add_library()
-    elif choice == '9':
-        add_book()
-    else:
-        menu()
+    choice = '0'
+
+    saved_library = open_file()
+
+    if saved_library == ' ':
+        library = Library("Smithsonian")
+
+    
+    while choice != '5':
+        choice = menu()
+        if choice == '1':
+            library.add_book()
+        elif choice == '2':
+            book_title = input("Enter book tittle to be borrowed:\t")
+            library.borrow_book(book_title)
+        elif choice == '3':
+            library.return_book()
+        elif choice == '4':
+            library.list_books()
+        else:
+            print("Enter a valid option")
+
+    print("Saving library...")
+    with open('all_libraries.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(library)
+    print("Exiting Program")
 
 def menu():
     print("** Electronic Library system **")
@@ -33,18 +42,9 @@ def menu():
     choice = input('')
     return choice
 
-def add_book():
-    book_title = input("Enter book title:\t")
-    book_author = input("Enter book author:\t")
-    book_ISBN = input("Enter book ISBN:\t")
-    book = library.Book(book_title, book_author, book_ISBN)
-    with open('all_books.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([book_title, book_author, book_ISBN])
-    
-def add_library():
-    library_name = input("Enter a name for library collection:\t")
-    
+def open_file():
+    with open('all_libraries.csv', 'w', newline='') as file:
+        return file
 
 if __name__ == "__main__":
     main()
